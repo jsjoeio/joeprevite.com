a856ce9ed6adfe43b57ab54aa2964c11402c676c
JavaScript Joe <jjprevite@gmail.com>
2019-11-28 14:22:19 -0700

    feat: update website info

--- config/website.js
+++ config/website.js
@@ -1,28 +1,29 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
+  siteTitle: 'Joe Previte - Developer &amp; Teacher', // Navigation and Site Title
+  siteTitleAlt: 'The blog of a developer and teacher', // Alternative Site title for SEO
+  siteTitleShort: 'Joe Previte', // short_name for manifest
   siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
+  siteDescription:
+    'Joe Previte is a full-stack developer and teacher specializing JavaScript and TypeScript.',
   author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  organization: 'jsjoeio',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
-  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  userTwitter: '@jsjoeio', // Twitter Username
+  ogSiteName: 'Joe Previte', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
 
   // Manifest and Progress color
-  themeColor: '#5348FF',
-  backgroundColor: '#2b2e3c',
+  themeColor: '#B63D3D',
+  backgroundColor: '#ffffff',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
+  twitter: 'https://twitter.com/jsjoeio/',
+  twitterHandle: '@jsjoeio',
+  github: 'https://github.com/jsjoeio/',
   linkedin: '',
 }
