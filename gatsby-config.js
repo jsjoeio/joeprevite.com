518a7910675bd551cc6576b85046d117c475f522
JavaScript Joe <jjprevite@gmail.com>
2019-12-06 18:58:33 -0700

    refactor: update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,9 +19,7 @@ module.exports = {
     author: {
       name: config.author,
       minibio: `
-        <strong>egghead</strong> is the premier place on the internet for
-        experienced developers to enhance their skills and stay current
-        in the fast-faced field of web development.
+        <strong>Joe Previte</strong> is a full-stack developer and teacher specializing JavaScript and TypeScript.
       `,
     },
     organization: {
