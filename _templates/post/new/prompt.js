b262015193b12fd60d6c49191585b98bc3301f1c
Joe Previte <jjprevite@gmail.com>
2020-01-15 13:59:38 -0700

    feat: add tagline to prompt and open in code

--- _templates/post/new/prompt.js
+++ _templates/post/new/prompt.js
@@ -1,3 +1,4 @@
+const { execSync } = require('child_process')
 const { slugify } = require('./slugify')
 const dayjs = require('dayjs')
 const open = require('open')
@@ -23,15 +24,25 @@ module.exports = {
             message: 'What is the tagline (for social image)?',
           },
         ])
-        .then(({ title, description }) => {
+        .then(({ title, description, tagline }) => {
           // Source: https://tecadmin.net/get-current-date-time-javascript/
           const date = dayjs().format('YYYY-MM-DD')
           const slug = slugify(title)
           const folderName = `${date}-${slug}`
           resolve({ title, description, tagline, date, slug, folderName })
-          return slug
+          return { slug, folderName }
         })
-        .then(slug => {
+        .then(({ slug, folderName }) => {
+          const pathToFile = `content/blog/${folderName}/index.md`
+          // Assuming we have yet to start the dev server
+
+          // Open the markdown file in VSCode
+          try {
+            execSync(`code ${pathToFile}`)
+          } catch (error) {
+            console.error('Oh no, something went wrong.')
+          }
+
           // Open in default browser
           open(`http://localhost:8000/${slug}`)
         })
