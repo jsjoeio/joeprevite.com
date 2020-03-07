09d7b308b72f79ddfa5bcaee8ddab3af3d712294
JavaScript Joe <jjprevite@gmail.com>
2020-03-07 12:01:23 -0800

    feat: add noctiusMinimus theme

---
+++ src/lib/theme.js
@@ -0,0 +1,88 @@
+// Original: https://github.com/liviuschera/noctis
+// Converted automatically using ./tools/themeFromVsCode
+var theme = {
+  plain: {
+    color: '#c5cdd3',
+    backgroundColor: '#1b2932',
+  },
+  styles: [
+    {
+      types: ['comment'],
+      style: {
+        color: 'rgb(94, 120, 135)',
+      },
+    },
+    {
+      types: ['punctuation', 'keyword', 'attr-name'],
+      style: {
+        color: 'rgb(200, 141, 162)',
+      },
+    },
+    {
+      types: ['char'],
+      style: {
+        color: 'rgb(197, 205, 211)',
+      },
+    },
+    {
+      types: ['constant', 'number'],
+      style: {
+        color: 'rgb(112, 104, 177)',
+      },
+    },
+    {
+      types: ['operator'],
+      style: {},
+    },
+    {
+      types: ['variable'],
+      style: {
+        color: 'rgb(63, 132, 141)',
+      },
+    },
+    {
+      types: ['builtin'],
+      style: {
+        color: 'rgb(211, 182, 146)',
+      },
+    },
+    {
+      types: ['string'],
+      style: {
+        color: 'rgb(63, 141, 108)',
+      },
+    },
+    {
+      types: ['symbol'],
+      style: {
+        color: 'rgb(190, 133, 111)',
+      },
+    },
+    {
+      types: ['function'],
+      style: {
+        color: 'rgb(168, 140, 0)',
+      },
+    },
+    {
+      types: ['tag', 'deleted'],
+      style: {
+        color: 'rgb(195, 116, 85)',
+      },
+    },
+    {
+      types: ['inserted'],
+      style: {
+        color: 'rgb(114, 192, 159)',
+      },
+    },
+    {
+      types: ['changed', 'namespace'],
+      style: {
+        color: 'rgb(114, 183, 192)',
+      },
+    },
+  ],
+}
+
+module.exports = theme
