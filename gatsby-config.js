f615c0734372c7bbea0501a8abf4ad52af68a34b
Joe Previte <jjprevite@gmail.com>
2020-04-26 16:57:40 -0700

    refactor: move everything to content

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/content/blog`,
-        name: 'blog',
+        path: `${__dirname}/content`,
+        name: '',
       },
     },
     {
