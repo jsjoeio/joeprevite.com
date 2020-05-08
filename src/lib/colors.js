90daac3791ad76b90242c2e17da48ca39c2fc3ab
Joe Previte <jjprevite@gmail.com>
2020-05-07 19:49:51 -0700

    feat: add support for Note, dark mode

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -9,6 +9,10 @@ const colors = {
   lightningYellow: '#F9B52A',
   saffron: '#F7CC42',
   transparent: 'transparent',
+  oasis: '#fcefcb',
+  goldenGlow: '#f8d573',
+  balticSea: '#2b2c2c',
+  raven: '#717273',
 }
 
 export default colors
