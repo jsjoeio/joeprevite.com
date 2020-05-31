d5de34d58e625e0af9c8b2b20767028101b114d8
Joe Previte <jjprevite@gmail.com>
2020-05-31 11:15:25 -0700

    feat: add webpack node externals

---
+++ config/webpack.functions.js
@@ -0,0 +1,7 @@
+// required to get firebase-sdk imported into netlify functions
+// see: https://github.com/netlify/netlify-lambda/issues/112#issuecomment-488644361
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  externals: [nodeExternals()],
+}
