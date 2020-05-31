537a5dc931a4e369d0a3cf0a9441c25e80350f09
Joe Previte <jjprevite@gmail.com>
2020-05-31 11:15:36 -0700

    feat: get basic page views working with json

--- functions/page-views.js
+++ functions/page-views.js
@@ -1,3 +1,13 @@
+const admin = require('firebase-admin')
+const serviceAccountKey = require('../serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+  databaseURL: 'https://website-pageviews-c8d4d.firebaseio.com/',
+})
+
+const db = admin.database()
+
 exports.handler = async (event, context) => {
   const { id } = event.queryStringParameters
   if (!id) {
@@ -9,11 +19,21 @@ exports.handler = async (event, context) => {
     }
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      pageId: id,
-      totalViews: 100,
-    }),
-  }
+  // This database has all my website page views
+  // so we need to look at the specific site
+  // then look at the views table which has {key: value} where key is the id for the page
+  const ref = db.ref('joeprevite-dot-com/views').child(id)
+
+  await ref.once('value', snapshot => {
+    // grab the total views
+    const totalViews = snapshot.val()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        pageId: id,
+        totalViews: totalViews !== null ? totalViews : 0,
+      }),
+    }
+  })
 }
