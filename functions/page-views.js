6bc528fc3f30b36974267a2fd3aa3fb01784c76a
Joe Previte <jjprevite@gmail.com>
2020-05-31 19:59:56 -0700

    feat: get all views from page views

--- functions/page-views.js
+++ functions/page-views.js
@@ -2,24 +2,32 @@ import { db } from '../src/lib/db-admin'
 
 exports.handler = async (event, context) => {
   const { id } = event.queryStringParameters
+
   if (!id) {
+    let totalViews
+    await db.ref('joeprevite-dot-com/views').once('value', snapshot => {
+      const views = snapshot.val()
+      // Grabs all the views for all posts
+      const allViews = Object.values(views).reduce(
+        (total, value) => total + value,
+      )
+      totalViews = allViews
+    })
     return {
-      statusCode: 400,
+      statusCode: 200,
       body: JSON.stringify({
-        error: 'Missing "id" query parameter',
+        totalViews: totalViews || 0,
       }),
     }
   }
+
   const ref = db.ref('joeprevite-dot-com/views').child(id)
 
   let totalViews
   await ref.once('value', snapshot => {
     const value = snapshot.val()
-    console.log('from firebase db:', value)
     totalViews = value
   })
-
-  console.log('here is total views', totalViews)
 
   return {
     statusCode: 200,
