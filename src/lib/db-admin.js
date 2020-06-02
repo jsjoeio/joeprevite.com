c72909ca5804a8bcec41b5023e6eb98f1f9d05ef
Joe Previte <jjprevite@gmail.com>
2020-06-01 19:28:25 -0700

    refactor: try parsing private key in prod

--- src/lib/db-admin.js
+++ src/lib/db-admin.js
@@ -37,10 +37,15 @@
 
 const admin = require('firebase-admin')
 
+// Due to the way the key is provided by Netlify
+// in production, we need to use the ternary
 const config = {
   credential: admin.credential.cert({
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key:
+      process.env.FIREBASE_PRIVATE_KEY[0] === '-'
+        ? process.env.FIREBASE_PRIVATE_KEY
+        : JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
     project_id: 'website-pageviews-c8d4d',
   }),
   databaseURL: 'https://website-pageviews-c8d4d.firebaseio.com/',
