fb1ee743a271c117811706949c3594b33413ac1b
Joe Previte <jjprevite@gmail.com>
2020-06-06 19:54:10 -0700

    fix: update debug and prod

--- functions/debug.js
+++ functions/debug.js
@@ -1,12 +1,10 @@
-import testConfig from '../src/lib/test'
-
 exports.handler = async (event, context) => {
   console.log('DEBUG: function has fired')
-  console.log(`this should show the private key: ${testConfig.privateKey}`)
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'debugging in prod',
+      message:
+        'SUCCESS! Functions are working properly if you see this message.',
     }),
   }
 }
