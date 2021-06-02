e10e8d00b9aaa129d13bffd4313e1d6355bc91dd
Alexey Berezin <beraliv.spb@gmail.com>
2021-06-02 22:24:39 +0200

    fix: 🧪 SubscribePage

--- src/pages/subscribe.tsx
+++ src/pages/subscribe.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import Layout, { LayoutPropsType } from '../components/Layout'
 import Container from '../components/Container'
 import Subscribe from '../components/Forms/Subscribe'
 import SEO from '../components/SEO'
 
-export default ({ data: { site } }) => (
+interface SubscribePagePropsType {
+  data: {
+    site: LayoutPropsType['site'];
+  }
+}
+
+const SubscribePage: FC<SubscribePagePropsType> = ({ data: { site } }) => (
   <Layout site={site} noFooter>
     <SEO />
     <Container>
@@ -13,6 +19,8 @@ export default ({ data: { site } }) => (
     </Container>
   </Layout>
 )
+
+export default SubscribePage;
 
 export const pageQuery = graphql`
   query {
