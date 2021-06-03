163181ab92fa9b402b32b1854f865d58c284627c
Alexey Berezin <beraliv.spb@gmail.com>
2021-06-03 12:21:45 +0200

    fix: 🧪 remove unused imports

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React from 'react'
 import { graphql, PageProps } from 'gatsby'
 import Layout from '../components/Layout'
 import Message from '../components/ConfirmMessage/Message'
--- src/pages/open.tsx
+++ src/pages/open.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React from 'react'
 import { graphql, PageProps } from 'gatsby'
 import useSWR from 'swr'
 import Container from '../components/Container'
