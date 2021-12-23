f6d4e9a8f8a8cad801b5d7a92126802908dbfdea
Joe <joe@coder.com>
2021-12-23 05:25:21 +0000

    feat: add footer

--- astro-migration/src/data/site.ts
+++ astro-migration/src/data/site.ts
@@ -1,11 +1,17 @@
 export default {
-    title: `Joe Previte`,
-    description: `This is an example blog made with Astro.`,
-    url: `https://jsjoeio-astro-starter.netlify.app`, // No trailing slash!
-    blogPath: `/`, // Some people might use /blog/, include trailing slash!
-    aboutPath: `/`,
-    // JSON LD
-    name: `Joe Previte`,
-    // Twitter URL
-    twitterURL: `https://twitter.com/jsjoeio`,
+  title: `Joe Previte`,
+  description: `This is an example blog made with Astro.`,
+  url: `https://jsjoeio-astro-starter.netlify.app`, // No trailing slash!
+  blogPath: `/`, // Some people might use /blog/, include trailing slash!
+  aboutPath: `/`,
+  // JSON LD
+  name: `Joe Previte`,
+  // Twitter URL
+  twitterURL: `https://twitter.com/jsjoeio`,
+  // GitHub URL
+  githubURL: `https://github.com/jsjoeio`,
+  // LinkedIn URL
+  linkedinURL: `https://linkedin.com/in/jsjoeio`,
+  // YouTube URL
+  youtubeURL: `https://youtube.com/c/jsjoeio`,
 }
