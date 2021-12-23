c61fa0a1b2682724f35db11ac399e31d0365ec1b
Joe <joe@coder.com>
2021-12-23 05:36:30 +0000

    feat: add newsletter

---
+++ astro-migration/src/components/Newsletter.tsx
@@ -0,0 +1,60 @@
+const description =
+  'I send a monthly newsletter with 1 exciting thing, 1 helpful thing, fun internet findings and new jobs. Subscribe for exclusive goodies!'
+function Newsletter() {
+  return (
+    <div>
+      <p className="mt-0 mb-2">{description}</p>
+      <div id="revue-embed">
+        <form
+          action="https://www.getrevue.co/profile/jsjoeio/add_subscriber"
+          method="post"
+          id="revue-form"
+          name="revue-form"
+          className="revue-form-group"
+          target="_blank"
+        >
+          <label htmlFor="member_email">
+            <div className="flex justify-between items-end">Email*</div>
+            <input
+              required
+              className="revue-form-field"
+              placeholder="awesomeperson@gmail.com"
+              type="email"
+              name="member[email]"
+              id="member_email"
+            />
+          </label>
+
+          <input
+            className="revue-form-actions w-auto"
+            type="submit"
+            value="Sign me up!"
+            name="member[subscribe]"
+            id="member_submit"
+          />
+        </form>
+        <small className="revue-form-footer block mt-2">
+          By subscribing, you agree with Revue’s{' '}
+          <a
+            className="hover:text-blue-800 underline"
+            target="_blank"
+            href="https://www.getrevue.co/terms"
+          >
+            Terms of Service
+          </a>{' '}
+          and{' '}
+          <a
+            className="hover:text-blue-800 underline"
+            target="_blank"
+            href="https://www.getrevue.co/privacy"
+          >
+            Privacy Policy
+          </a>
+          .
+        </small>
+      </div>
+    </div>
+  )
+}
+
+export default Newsletter
